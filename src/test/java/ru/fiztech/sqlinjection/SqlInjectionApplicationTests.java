3da52fbd4f2d88e5fdfd1a25bfd26b465dc2cec8
tip <aik442557@gmail.com>
2019-06-14 01:22:45 +0300

    init

---
+++ src/test/java/ru/fiztech/sqlinjection/SqlInjectionApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.fiztech.sqlinjection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SqlInjectionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
